b0d14d2ef99177431b78f5ef289c471e6ba4831a
Briann254 <bnnderitu01@gmail.com>
2025-04-02 17:04:00 +0300

    Make the cart quantity Interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -120,7 +120,17 @@ let matchingItem;
       });
     }
 
+    let cartQuantity = 0;
+
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
     
+    console.log(cartQuantity);
     console.log(cart);
   });
 });
